6f43324226fd1bedab4281072bb82b6a2c6f00b0
Joshua-Xavier <j.nelssonsmith@gmail.com>
2016-10-06 20:38:46 +1100

    completed lecture 8 work, added a UserDetailsWrapper component:

--- githubBattle/app/components/ConfirmBattle.js
+++ githubBattle/app/components/ConfirmBattle.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import styles from '../styles/styles'
 import { Link } from 'react-router'
 import UserDetails from '../components/UserDetails'
+import UserDetailsWrapper from '../components/UserDetailsWrapper'
 
 function puke(object) {
     return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -17,14 +18,12 @@ function ConfirmBattle(props) {
             <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
                 <h1>Confirm Players</h1>
                 <div className="col-sm-8 col-sm-offset-2">
-                    <div className="col-sm-6">
-                        <p className="lead">Player 1</p>
+                    <UserDetailsWrapper header='Player One'>
                         <UserDetails info={props.playersInfo[0]} />
-                    </div>
-                    <div className="col-sm-6">
-                        <p className="lead">Player 2</p>
+                    </UserDetailsWrapper>
+                    <UserDetailsWrapper header='Player Two'>
                         <UserDetails info={props.playersInfo[1]} />
-                    </div>
+                    </UserDetailsWrapper>
                 </div>
                 <div className="col-sm-8 col-sm-offset-2">
                     <div className="col-sm-12" style={styles.space}>
---
+++ githubBattle/app/components/UserDetailsWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserDetailsWrapper(props) {
+    return (
+        <div className="col-sm-6">
+            <p className="lead">{props.header}</p>
+            {props.children}
+        </div>
+
+    )
+}
+
+export default UserDetailsWrapper
